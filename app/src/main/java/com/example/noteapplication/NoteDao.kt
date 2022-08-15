90582e0ab4bfa204da53f72a695b13c6e46f75c4
taufik0910 <taufik.cradle@gmail.com>
2022-08-16 04:22:42 +0700

    create class NoteDatabase.kt (00:14:01)

--- app/src/main/java/com/example/noteapplication/NoteDao.kt
+++ app/src/main/java/com/example/noteapplication/NoteDao.kt
@@ -1,4 +1,19 @@
 package com.example.noteapplication
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update()
+
+    @Delete
+    suspend fun delete()
+    @Query("select * from noteTable order by id ASC")
+    fun getAllNote():LiveData<List<Note>>
+
 }
