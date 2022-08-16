76c91a84cdb70299ed0edcff846e97eff25278a6
taufik0910 <taufik.cradle@gmail.com>
2022-08-17 02:55:00 +0700

    insert coding NoteRVAdapter.kt

--- app/src/main/java/com/example/noteapplication/MainActivity.kt
+++ app/src/main/java/com/example/noteapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.noteapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notesRV = findViewById(R.id.idRVNotes)
+        addFAB = findViewById(R.id.idFABAddNote)
     }
 }
